f7d3d7f2a3700e8c2ae9bc271c566970a446cfa6
mattymatty <matteo.francesconi@freemails.it>
2023-09-16 23:07:15 +0200

    bump

--- src/main/java/com/mattymatty/spacebarcrafting/mixins/RecipeUnlockerMixin.java
+++ src/main/java/com/mattymatty/spacebarcrafting/mixins/RecipeUnlockerMixin.java
@@ -2,6 +2,7 @@ package com.mattymatty.spacebarcrafting.mixins;
 
 import com.mattymatty.spacebarcrafting.SpaceBarCrafting;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeUnlocker;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,11 +11,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(RecipeUnlocker.class)
 public interface RecipeUnlockerMixin{
     @Shadow Recipe<?> getLastRecipe();
     @Inject(method = "unlockLastRecipe", at = @At("HEAD"))
-    default void save_last_recipe(PlayerEntity player, CallbackInfo ci){
+    default void save_last_recipe(PlayerEntity player, List<ItemStack> ingredients, CallbackInfo ci){
         SpaceBarCrafting.lastCraftedRecipe = getLastRecipe();
     }
 }
